4420dd767b847b3e90949469b48dff4fad73b962
jteck <jteck@uos.de>
2019-08-22 13:34:16 +0200

    set BonusPackHelper

--- app/src/main/java/com/example/firstapp/customRoadManager.java
+++ app/src/main/java/com/example/firstapp/customRoadManager.java
@@ -3,10 +3,19 @@ package com.example.firstapp;
 import android.content.Context;
 
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.utils.BonusPackHelper;
+
 
 public class customRoadManager extends OSRMRoadManager {
 
     public customRoadManager(Context context){
         super(context);
+       // mContext = context;
+        mServiceUrl = SERVICE;
+        mUserAgent = BonusPackHelper.DEFAULT_USER_AGENT;
     }
+
+    static final String SERVICE = "https://api.openrouteservice.org/v2/directions/driving-car";
+
+
 }
